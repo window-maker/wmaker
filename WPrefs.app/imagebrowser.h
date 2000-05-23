3e0c6920a92dab8f01b79638598aa1c92c56f397
kojima <kojima>
2000-05-23 21:22:38 +0000

    added it

---
+++ WPrefs.app/imagebrowser.h
@@ -0,0 +1,28 @@
+
+
+typedef struct _ImageBrowser ImageBrowser;
+typedef struct _ImageBrowserDelegate ImageBrowserDelegate;
+
+
+
+
+
+struct _ImageBrowserDelegate {
+    void *data;
+
+    void (*selected)(ImageBrowserDelegate *self,
+		     ImageBrowser *browser,
+		     char *path);
+    
+    void (*closed)(ImageBrowserDelegate *self,
+		   ImageBrowser *browser,
+		   Bool canceled);
+    
+};
+
+
+ImageBrowser *CreateImageBrowser(WMScreen *scr, char *title, char **paths,
+				 int pathN, WMSize *maxSize,
+				 WMWidget *auxWidget);
+
+
