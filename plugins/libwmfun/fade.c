4fb6cd1b1ed099b8f66956f1f8d1047162186e73
dan <dan>
2001-04-22 02:15:23 +0000

    Removed the last commit comment (a very lenghty one) that was added by cvs to this file, probably because of the presence of a RCS $Id entry

--- plugins/libwmfun/fade.c
+++ plugins/libwmfun/fade.c
@@ -18,69 +18,6 @@
  *  USA.
  */
 
-/*
- * $Id$
- *
- * $Log$
- * Revision 1.2  2001/04/21 07:12:30  dan
- * For libwraster:
- * ---------------
- *
- * - Added retain/release mechanism to RImage by adding RRetainImage() and
- *   RReleaseImage(). RDestroyImage() is an alias to RReleaseImage() now, but
- *   will be removed in a future release because it no longer fits with the
- *   semantics. Will be kept for a while to allow a smoother transition.
- *   More about in wrlib/NEWS
- *
- *
- * For WINGs:
- * ----------
- *
- * - Small API change:
- *   1. Renamed WMSetApplicationIconImage(), WMGetApplicationIconImage() and
- *      WMSetWindowMiniwindowImage() to respectively WMSetApplicationIconPixmap(),
- *      WMGetApplicationIconPixmap() and WMSetWindowMiniwindowPixmap()
- *      They operate on a WMPixmap which is practically an X Pixmap with no alpha
- *      channel information and the new name is more suggestive and also leaves
- *      room for the new functions added for operating on images with alpha info.
- *   2. Added WMSetApplicationIconImage() and WMGetApplicationIconImage() which
- *      operate on an RImage and store alpha information too.
- *   3. Added WMGetApplicationIconBlendedPixmap() which will take the image with
- *      alpha set by WMSetApplicationIconImage() and will blend it with a color.
- *      If color is NULL it will blend using the default panel color (#aeaaae)
- *   All these changes will allow WINGs to handle images with alpha blending
- *   correctly in panels and wherever else needed. More about in WINGs/NEWS.
- * - updated panels to use the newly available RImages if present and fallback
- *   to old WMPixmaps if not, to properly show alpha blended images.
- * - replaced some still left malloc's with wmalloc's.
- *
- *
- * For Window Maker:
- * -----------------
- * - Fixed wrong mapping position of the "Docked Applications Panel" for some
- *   icons.
- * - Smoother animation for the smiley =)
- * - Made images with alpha blending be shown correctly in the panels and the
- *   icon chooser.
- * - The icon image set to be shown in panels ("Logo.WMPanel") will be
- *   automatically updated if its entry in WMWindowAttributes changes (without
- *   a need to restart as until now).
- *
- *
- * *** Note!!! ***
- *
- * If you are developing applications with one of libwraster or libWINGs
- * then you should look to wrlib/NEWS and WINGs/NEWS to see what changed
- * and how should you update your code.
- *
- * Revision 1.1  2000/12/03 18:58:41  id
- * initiate plugins branch
- *
- * Revision 1.1.1.1  1999/02/21 17:16:47  gloth
- * initial revision
- *
- */
-
 #include "getopt.h"
 #include <math.h>
 #include <stdlib.h>
