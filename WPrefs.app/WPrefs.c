1e3a40370d6217e426fef36c83ddce169f4bcd48
kojima <kojima>
2004-10-18 01:37:17 +0000

    some documentation updates

--- WPrefs.app/WPrefs.c
+++ WPrefs.app/WPrefs.c
@@ -61,8 +61,8 @@ extern Panel *InitAppearance(WMScreen *scr, WMWidget *parent);
 
 
 
-#define ICON_TITLE_FONT "-adobe-helvetica-bold-r-*-*-10-*"
-#define ICON_TITLE_VFONT "-adobe-helvetica-bold-r-*-*-10-[]-*"
+#define ICON_TITLE_FONT "sans:pixelsize=9"
+#define ICON_TITLE_VFONT "sans:pixelsize=9:weight=100"
 
 
 #define MAX_SECTIONS 16
