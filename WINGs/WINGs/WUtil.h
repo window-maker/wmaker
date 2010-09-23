d2853c858407d19fecdd530c566e4319038e49ba
Tamas TEVESZ <ice@extreme.hu>
2010-09-23 23:23:29 +0200

    WINGs: Add detection and local copy for strlcat()/strlcpy()
    Signed-off-by: Tamas TEVESZ <ice@extreme.hu>

--- WINGs/WINGs/WUtil.h
+++ WINGs/WINGs/WUtil.h
@@ -849,7 +849,12 @@ extern int WCErrorCode;
 
 /*-------------------------------------------------------------------------*/
 
-
+#ifndef HAVE_STRLCPY
+size_t strlcpy(char *, const char *, size_t);
+#endif
+#ifndef HAVE_STRLCAT
+size_t strlcat(char *, const char *, size_t);
+#endif
 
 #ifdef __cplusplus
 }
