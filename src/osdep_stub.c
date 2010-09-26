c5f5e0b9c07db08003dd54494ad91ec380194632
Tamas TEVESZ <ice@extreme.hu>
2010-09-26 17:36:47 +0200

    More precise information on unknown systems
    Signed-off-by: Tamas TEVESZ <ice@extreme.hu>

--- src/osdep_stub.c
+++ src/osdep_stub.c
@@ -1,24 +1,29 @@
 
+#include <sys/utsname.h>
+
 #include <WINGs/WUtil.h>
 
 #include "wconfig.h"
 
-#ifndef STUB_HINT
-#define STUB_HINT	"(unknown)"
-#endif
-
 Bool GetCommandForPid(int pid, char ***argv, int *argc)
 {
-	*argv = NULL;
-	*argc = 0;
 	static int notified = 0;
 
 	if (!notified) {
-		wwarning(_("%s is not implemented on this platform; "
-		    "tell wmaker-dev@lists.windowmaker.info you are running \"%s\""),
-		    __FUNCTION__, STUB_HINT);
-		notified = 1;
+		struct utsname un;
+
+		if (uname(&un) != -1) {
+			wwarning(_("%s is not implemented on this platform; "
+				"tell wmaker-dev@windowmaker.org you are running "
+				"%s release %s version %s"), __FUNCTION__,
+				un.sysname, un.release, un.version);
+			notified = 1;
+		}
+
 	}
+
+	*argv = NULL;
+	*argc = 0;
 
 	return False;
 }
