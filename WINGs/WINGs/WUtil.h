7c98f7cfc464a67e4bb66925f830f70c54801eba
Christophe CURIS <christophe.curis@free.fr>
2013-05-10 18:35:44 +0200

    Added proper legal notice at beginning of header files
    Some header were missing the notice; used git blame to find the
    original date and author (without guarantee.

--- WINGs/WINGs/WUtil.h
+++ WINGs/WINGs/WUtil.h
@@ -1,3 +1,24 @@
+/* WUtil.h
+ *
+ *  Copyright (c) 1998 scottc
+ *  Copyright (c) 1999-2004 Dan Pascu
+ *  Copyright (c) 1999-2000 Alfredo K. Kojima
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #ifndef _WUTIL_H_
 #define _WUTIL_H_
 
