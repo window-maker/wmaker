8b1babc5b36bafdc4f46da8176a438587e14af08
Doug Torrance <dtorrance@monmouthcollege.edu>
2013-11-01 19:12:31 -0500

    Remove newlines in legal panel.
    The label containing the legal information in the legal panel was set to wrap.
    However, there were redundant newlines in the text itself.  In wider fonts, this
    had the effect of the legal text not completely fitting in the legal panel.
    
    This patch removes the unnecessary newlines.

--- src/dialog.c
+++ src/dialog.c
@@ -1419,17 +1419,17 @@ void wShowLegalPanel(WScreen * scr)
 	WMMoveWidget(panel->licenseL, 10, 10);
 	WMSetLabelTextAlignment(panel->licenseL, WALeft);
 	WMSetLabelText(panel->licenseL,
-		       _("    Window Maker is free software; you can redistribute it and/or\n"
-			 "modify it under the terms of the GNU General Public License as\n"
-			 "published by the Free Software Foundation; either version 2 of the\n"
+		       _("    Window Maker is free software; you can redistribute it and/or "
+			 "modify it under the terms of the GNU General Public License as "
+			 "published by the Free Software Foundation; either version 2 of the "
 			 "License, or (at your option) any later version.\n\n"
-			 "    Window Maker is distributed in the hope that it will be useful,\n"
-			 "but WITHOUT ANY WARRANTY; without even the implied warranty\n"
-			 "of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n"
+			 "    Window Maker is distributed in the hope that it will be useful, "
+			 "but WITHOUT ANY WARRANTY; without even the implied warranty "
+			 "of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. "
 			 "See the GNU General Public License for more details.\n\n"
-			 "    You should have received a copy of the GNU General Public\n"
-			 "License along with this program; if not, write to the Free Software\n"
-			 "Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA\n"
+			 "    You should have received a copy of the GNU General Public "
+			 "License along with this program; if not, write to the Free Software "
+			 "Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA"
 			 "02110-1301 USA."));
 	WMSetLabelRelief(panel->licenseL, WRGroove);
 
