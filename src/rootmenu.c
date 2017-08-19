5f2dac318cdafc10b5696f069951b9119314523f
Doug Torrance <dtorrance@piedmont.edu>
2017-08-18 20:37:49 -0400

    menu: New macro #usergnusteppath# for user GNUstep directory
    This macro is used when handling directories with OPEN_MENU, e.g., to list
    available image files for setting the workspace background.
    
    When parsing a menu file, we replace any instances of #usergnusteppath#
    with either GNUSTEP_USER_ROOT or ~/GNUstep if the former is not set.  In
    this way, authors of menu files do not have to worry about whether users
    will have this variable set or not.
    
    We also document this feature in WindowMaker/menu.in, which generates the
    default English language old-style menu and currently contains the existing
    documentation for the Window Maker menu system.

--- src/rootmenu.c
+++ src/rootmenu.c
@@ -673,6 +673,11 @@ static void constructMenu(WMenu * menu, WMenuEntry * entry)
 				}
 
 				tmp = wexpandpath(path[i]);
+
+				if (strstr(tmp, "#usergnusteppath#") == tmp)
+					tmp = wstrconcat(wusergnusteppath(),
+							  tmp + 17);
+
 				wfree(path[i]);
 				lpath = getLocalizedMenuFile(tmp);
 				if (lpath) {
