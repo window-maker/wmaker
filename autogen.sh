4edf589a5b02d704785ac1f4d3896981898943bb
Doug Torrance <dtorrance@piedmont.edu>
2024-11-11 15:11:03 -0500

    Remove cruft from autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -14,18 +14,3 @@ touch -d '2000-01-01' $srcdir/INSTALL-WMAKER $srcdir/README.i18n
 
 # Generate the configure script from the 'configure.ac'
 autoreconf -vfi -I m4 $srcdir
-
-exit 0
-
-if [ -x config.status -a -z "$*" ]; then
-  ./config.status --recheck
-else
-  if test -z "$*"; then
-    echo "I am going to run ./configure with no arguments - if you wish "
-    echo "to pass any to it, please specify them on the $0 command line."
-    echo "If you do not wish to run ./configure, press Ctrl-C now."
-    trap 'echo "configure aborted" ; exit 0' 1 2 15
-    sleep 1
-  fi
-  ./configure "$@"
-fi
